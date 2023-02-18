641919961ece39f1277f11f5d971978ae84f8a9e
Mrbysco <Mrbysco@users.noreply.github.com>
2023-02-18 02:31:47 +0100

    Update DurabilityNotifier.java

--- Forge/src/main/java/com/mrbysco/durabilitynotifier/DurabilityNotifier.java
+++ Forge/src/main/java/com/mrbysco/durabilitynotifier/DurabilityNotifier.java
@@ -3,12 +3,13 @@ package com.mrbysco.durabilitynotifier;
 import com.mrbysco.durabilitynotifier.config.DurabilityConfig;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -23,14 +24,17 @@ public class DurabilityNotifier {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, DurabilityConfig.clientSpec);
 		FMLJavaModLoadingContext.get().getModEventBus().register(DurabilityConfig.class);
 
-		MinecraftForge.EVENT_BUS.addListener(this::onLeftClickBlock);
-		MinecraftForge.EVENT_BUS.addListener(this::onLeftClickEmpty);
-		MinecraftForge.EVENT_BUS.addListener(this::onRightClickBlock);
-		MinecraftForge.EVENT_BUS.addListener(this::onAttackEntity);
-		MinecraftForge.EVENT_BUS.addListener(this::onInventoryTick);
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+			MinecraftForge.EVENT_BUS.addListener(this::onLeftClickBlock);
+			MinecraftForge.EVENT_BUS.addListener(this::onLeftClickEmpty);
+			MinecraftForge.EVENT_BUS.addListener(this::onRightClickBlock);
+			MinecraftForge.EVENT_BUS.addListener(this::onAttackEntity);
+			MinecraftForge.EVENT_BUS.addListener(this::onInventoryTick);
+		});
 
 		//Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "Trans Rights Are Human Rights", (remoteVersionString, networkBool) -> networkBool));
+		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () ->
+				new IExtensionPoint.DisplayTest(() -> "Trans Rights Are Human Rights", (remoteVersionString, networkBool) -> networkBool));
 	}
 
 	private void onLeftClickBlock(final PlayerInteractEvent.LeftClickBlock event) {
@@ -54,8 +58,7 @@ public class DurabilityNotifier {
 		if (event.phase == TickEvent.Phase.START) return;
 
 		Player player = event.player;
-		Level level = player.level;
-		if (level.isClientSide && player.level.getGameTime() % 80 == 0) {
+		if (player.level.getGameTime() % 80 == 0) {
 			if (DurabilityConfig.CLIENT.CheckArmor.get()) {
 				for (ItemStack itemStack : player.getInventory().armor) {
 					EventHandler.checkDurability(itemStack, player);
