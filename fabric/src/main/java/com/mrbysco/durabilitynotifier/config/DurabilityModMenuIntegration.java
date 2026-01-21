bc895218bf00d036d9c236f87f3d6ae58e7d1b0f
Mrbysco <Mrbysco@users.noreply.github.com>
2026-01-21 17:35:57 +0100

    Switch AutoConfig class (deprecation)

--- fabric/src/main/java/com/mrbysco/durabilitynotifier/config/DurabilityModMenuIntegration.java
+++ fabric/src/main/java/com/mrbysco/durabilitynotifier/config/DurabilityModMenuIntegration.java
@@ -3,10 +3,11 @@ package com.mrbysco.durabilitynotifier.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class DurabilityModMenuIntegration implements ModMenuApi {
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return parent -> AutoConfig.getConfigScreen(DurabilityConfig.class, parent).get();
+		return parent -> AutoConfigClient.getConfigScreen(DurabilityConfig.class, parent).get();
 	}
 }
